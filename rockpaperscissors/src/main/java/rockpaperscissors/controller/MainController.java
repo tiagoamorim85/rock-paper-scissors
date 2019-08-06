7cb1a157976be6250fb8d989bb99211bd830ce78
Tiago Amorim <tiago.amorim85@gmail.com>
2019-08-06 21:10:29 +0200

    Refactor

--- rockpaperscissors/src/main/java/rockpaperscissors/controller/MainController.java
+++ rockpaperscissors/src/main/java/rockpaperscissors/controller/MainController.java
@@ -1,8 +1,5 @@
 package rockpaperscissors.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
